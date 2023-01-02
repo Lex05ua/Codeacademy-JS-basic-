523957220fc751ddf5a1701ed60d6ddd3be59b95
lexuss05Od <93823742+Lex05ua@users.noreply.github.com>
2023-01-02 12:16:49 +0200

    Create Kelvin Weather.js

---
+++ Kelvin Weather/Kelvin Weather.js
@@ -0,0 +1,15 @@
+//declare Kelvin 
+const kelvin = 293; 
+ 
+//declare Celsius 
+const celsius = kelvin - 273; 
+ 
+//declare Fahrenheit 
+let fahrenheit = celsius * (9/5) + 32; 
+ 
+/* 
+Use the .floor() method to round down the Fahrenheit temperature. 
+ */ 
+Math.floor(fahrenheit); 
+ 
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
